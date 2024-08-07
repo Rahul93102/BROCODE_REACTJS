2b9b47f8f3fa644e9872a1f1533d4bc8eea5af5a
Rahul_jha <rahuljha996886@gmail.com>
2024-08-08 04:21:35 +0530

    Accordian

--- BROCODE_REACTJS/M_1/Check.jsx
+++
@@ -1,7 +0,0 @@
-function Check() { 
-    return ( 
-        <span>Working on something</span>
-    );
-}
-
-export default Check;
--- BROCODE_REACTJS/M_5/UserGretting.jsx
+++
@@ -1,7 +0,0 @@
-function UserGreetting(props) { 
-    return (
-        props.isLoggedIn ? <h2>Welcome {props.username},</h2> : <h2>Please login {props.username}</h2>
-    );
-}
-
-export default UserGreetting;
